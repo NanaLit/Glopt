600e2a9e587837299fbf69e83af1cb1557f12ac2
Nana <zubiloandphima@gmail.com>
2022-01-01 11:21:25 +0300

    mailer_glopt

--- dist/js/script.js
+++ dist/js/script.js
@@ -19,4 +19,57 @@ $(document).ready(function(){
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/all/left.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/all/right.png"></button>'
     });
+
+    function validateForms(form){
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Введите {0} символа!")
+                  },
+                phone: {
+                    required: "Пожалуйста, введите свой номер телефона"
+                },
+                email: {
+                  required: "Пожалуйста, введите свою почту",
+                  email: "Неправильно введен адрес почты"
+                }
+            }
+        });
+    };
+    validateForms('#consultation-form');
+    validateForms('#consultation');
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+
+        if (!$(this).valid()) {
+            return;
+        }
+        
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('form').trigger('reset');
+        });
+        return false;
+    });
 });
+
+
